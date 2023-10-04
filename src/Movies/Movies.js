cc6c79de75dbda11622c00dec1098e9ce7c96a2c
Aditya M <aditya.m@customerxps.com>
2023-10-04 19:54:56 +0530

    added

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Tables from "./Tables";
-import axios from "axios";
+// import axios from "axios";
 import AddMovies from "./AddMovies";
 import moviesData from "./movie.json"; // Adjust the path as needed
 
